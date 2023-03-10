dec653908fae8da89cd073cf2998f3b3da5dc479
LucasBrunner <lucasbrunner@gmail.com>
2023-03-10 14:31:05 -0500

    convert rapid stop slew rate limiter to use bindable values

--- src/main/java/frc/robot/utility/RapidStopSlewRateLimiter.java
+++ src/main/java/frc/robot/utility/RapidStopSlewRateLimiter.java
@@ -4,18 +4,41 @@ public class RapidStopSlewRateLimiter implements SlewRateLimiter {
 
     private double previousValue;
 
-    private double maxPositiveMagnitudeChange;
-    private double maxNegativeMagnitudeChange;
+    private BindableValue<Double> maxPositiveMagnitudeChange;
+    private BindableValue<Double> maxNegativeMagnitudeChange;
 
-    private double maxPositiveMagnitudeStopChange;
-    private double maxNegativeMagnitudeStopChange;
+    private BindableValue<Double> maxPositiveMagnitudeStopChange;
+    private BindableValue<Double> maxNegativeMagnitudeStopChange;
 
-    public RapidStopSlewRateLimiter(double maxPositiveMagnitudeChange, double maxNegativeMagnitudeChange, double maxPositiveMagnitudeStopChange, double maxNegativeMagnitudeStopChange) {
-        this.maxPositiveMagnitudeChange = Math.abs(maxPositiveMagnitudeChange);
-        this.maxNegativeMagnitudeChange = Math.abs(maxNegativeMagnitudeChange);
+    /**
+     * All magnitudes should give positive values.
+     * @param maxPositiveMagnitudeChange
+     * @param maxNegativeMagnitudeChange
+     * @param maxPositiveMagnitudeStopChange
+     * @param maxNegativeMagnitudeStopChange
+     */
+    public RapidStopSlewRateLimiter(
+            BindableValue<Double> maxPositiveMagnitudeChange,
+            BindableValue<Double> maxNegativeMagnitudeChange,
+            BindableValue<Double> maxPositiveMagnitudeStopChange,
+            BindableValue<Double> maxNegativeMagnitudeStopChange) {
+        this.maxPositiveMagnitudeChange = maxPositiveMagnitudeChange;
+        this.maxNegativeMagnitudeChange = maxNegativeMagnitudeChange;
 
-        this.maxPositiveMagnitudeStopChange = Math.abs(maxPositiveMagnitudeStopChange);
-        this.maxNegativeMagnitudeStopChange = Math.abs(maxNegativeMagnitudeStopChange);
+        this.maxPositiveMagnitudeStopChange = maxPositiveMagnitudeStopChange;
+        this.maxNegativeMagnitudeStopChange = maxNegativeMagnitudeStopChange;
+    }
+
+    public RapidStopSlewRateLimiter(
+            double maxPositiveMagnitudeChange,
+            double maxNegativeMagnitudeChange,
+            double maxPositiveMagnitudeStopChange,
+            double maxNegativeMagnitudeStopChange) {
+        this(
+                new ConstantBindableValue<Double>(Math.abs(maxPositiveMagnitudeChange)),
+                new ConstantBindableValue<Double>(Math.abs(maxNegativeMagnitudeChange)),
+                new ConstantBindableValue<Double>(Math.abs(maxPositiveMagnitudeStopChange)),
+                new ConstantBindableValue<Double>(Math.abs(maxNegativeMagnitudeStopChange)));
     }
 
     public RapidStopSlewRateLimiter(double maxChange, double maxStopChange) {
@@ -33,23 +56,25 @@ public class RapidStopSlewRateLimiter implements SlewRateLimiter {
 
     private double differentDirectionCalculation(double nextValue) {
         if (nextValue > 0) {
-            return setAndReturn(previousValue + Math.min(maxNegativeMagnitudeStopChange, Math.abs(previousValue)));
+            return setAndReturn(
+                    previousValue + Math.min(maxNegativeMagnitudeStopChange.value(), Math.abs(previousValue)));
         } else if (nextValue < 0) {
-            return setAndReturn(previousValue - Math.min(maxPositiveMagnitudeStopChange, Math.abs(previousValue)));
-        } 
+            return setAndReturn(
+                    previousValue - Math.min(maxPositiveMagnitudeStopChange.value(), Math.abs(previousValue)));
+        }
         return setAndReturn(0);
     }
 
     private double positivePower(double nextValue, double diff) {
         if (diff > 0) {
             // traveling forward and speeding up
-            if (diff > maxPositiveMagnitudeChange) {
-                return setAndReturn(previousValue + maxPositiveMagnitudeChange);
+            if (diff > maxPositiveMagnitudeChange.value()) {
+                return setAndReturn(previousValue + maxPositiveMagnitudeChange.value());
             }
         } else {
             // traveling forward and slowing down
-            if (-diff > maxPositiveMagnitudeStopChange) {
-                return setAndReturn(previousValue - maxPositiveMagnitudeStopChange);
+            if (-diff > maxPositiveMagnitudeStopChange.value()) {
+                return setAndReturn(previousValue - maxPositiveMagnitudeStopChange.value());
             }
         }
         return setAndReturn(nextValue);
@@ -58,13 +83,13 @@ public class RapidStopSlewRateLimiter implements SlewRateLimiter {
     private double negativePower(double nextValue, double diff) {
         if (diff < 0) {
             // traveling backward and speeding up
-            if (-diff > maxNegativeMagnitudeChange) {
-                return setAndReturn(previousValue - maxNegativeMagnitudeChange);
+            if (-diff > maxNegativeMagnitudeChange.value()) {
+                return setAndReturn(previousValue - maxNegativeMagnitudeChange.value());
             }
         } else {
             // traveling backward and slowing down
-            if (diff > maxNegativeMagnitudeStopChange) {
-                return setAndReturn(previousValue + maxNegativeMagnitudeStopChange);
+            if (diff > maxNegativeMagnitudeStopChange.value()) {
+                return setAndReturn(previousValue + maxNegativeMagnitudeStopChange.value());
             }
         }
         return setAndReturn(nextValue);
