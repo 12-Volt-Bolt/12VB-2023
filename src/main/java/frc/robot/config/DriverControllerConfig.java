75038ed4efbac3b69655e6237a83fa51ba8f60b6
LucasBrunner <lucasbrunner@gmail.com>
2023-03-15 17:24:40 -0400

    refactor driver controller config
    fix glitch where the wall detector prevented turbo mode from working

--- src/main/java/frc/robot/config/DriverControllerConfig.java
+++ src/main/java/frc/robot/config/DriverControllerConfig.java
@@ -1,15 +1,9 @@
 package frc.robot.config;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import frc.robot.DriverController;
 import frc.robot.subsystem.Lifter;
-import frc.robot.utility.BindableValue;
 import frc.robot.utility.DeadzoneWithLinearRemap;
 import frc.robot.utility.ExponentialRemap;
-import frc.robot.utility.Remapper;
-import frc.robot.utility.RemapperChain;
 
 public class DriverControllerConfig {
     private static DeadzoneWithLinearRemap deadzone() {
@@ -28,85 +22,20 @@ public class DriverControllerConfig {
         return turboRemapper;
     }
 
-    private static DeadzoneWithLinearRemap wallDetector(Lifter lifter, BindableValue<Double> maxInput) {
+    private static DeadzoneWithLinearRemap wallDetector(Lifter lifter) {
         DeadzoneWithLinearRemap wallDetectorRemapper = new DeadzoneWithLinearRemap(1.0, 1.0);
         wallDetectorRemapper.maxPositiveOutput = new WallDetectorBindableValue(lifter, 1.0, 0.25);
-        wallDetectorRemapper.maxPositiveInput = maxInput;
         return wallDetectorRemapper;
-    }
-
-    public static RemapperChain<Double> yInputRemappers(DriverController controller, Lifter lifter) {
-        List<Remapper<Double>> remappers = new ArrayList<>();
-
-        // deadzone
-        remappers.add(new DeadzoneWithLinearRemap(1, 0.2, 1));
-
-        // throttle curve
-        remappers.add(new ExponentialRemap(1.5, true));
-        
-        // turbo mode
-        DeadzoneWithLinearRemap turboRemapper = new DeadzoneWithLinearRemap(1.0, 0.0, 1.0);
-        TurboModeBindableValue turboOutputLimit = new TurboModeBindableValue(controller, 0.5, 1.0);
-        turboRemapper.maxPositiveOutput = turboOutputLimit.cloneValue();
-        turboRemapper.maxNegativeOutput = turboOutputLimit.cloneValue().negate();
-        remappers.add(turboRemapper);
-
-        // wall detection
-        DeadzoneWithLinearRemap wallDetectorRemapper = new DeadzoneWithLinearRemap(1.0, 1.0);
-        wallDetectorRemapper.maxPositiveOutput = new WallDetectorBindableValue(lifter, 1.0, 0.25);
-        wallDetectorRemapper.maxPositiveInput = turboOutputLimit.cloneValue();
-        remappers.add(wallDetectorRemapper);
-
-        return new RemapperChain<>(remappers);
-    }
-    
-    public static RemapperChain<Double> xInputRemappers(DriverController controller) {
-        List<Remapper<Double>> remappers = new ArrayList<>();
-
-        // deadzone
-        remappers.add(new DeadzoneWithLinearRemap(1, 0.2, 1));
-
-        // throttle curve
-        remappers.add(new ExponentialRemap(1.5, true));
-        
-        // turbo mode
-        DeadzoneWithLinearRemap turboRemapper = new DeadzoneWithLinearRemap(1.0, 0.0, 1.0);
-        TurboModeBindableValue turboOutputLimit = new TurboModeBindableValue(controller, 0.5, 1.0);
-        turboRemapper.maxPositiveOutput = turboOutputLimit.cloneValue();
-        turboRemapper.maxNegativeOutput = turboOutputLimit.cloneValue().negate();
-        remappers.add(turboRemapper);
-
-        return new RemapperChain<>(remappers);
-    }
-    
-    public static RemapperChain<Double> yawInputRemappers(DriverController controller) {
-        List<Remapper<Double>> remappers = new ArrayList<>();
-
-        // deadzone
-        remappers.add(new DeadzoneWithLinearRemap(1, 0.2, 1));
-
-        // throttle curve
-        remappers.add(new ExponentialRemap(1.5, true));
-        
-        // turbo mode
-        DeadzoneWithLinearRemap turboLimiter = new DeadzoneWithLinearRemap(1.0, 0.0, 1.0);
-        TurboModeBindableValue turboOutputLimit = new TurboModeBindableValue(controller, 0.7, 1.0);
-        turboLimiter.maxPositiveOutput = turboOutputLimit.cloneValue();
-        turboLimiter.maxNegativeOutput = turboOutputLimit.cloneValue().negate();
-        remappers.add(turboLimiter);
-
-        return new RemapperChain<>(remappers);
     }
 
     public static DriverController configDriverController(
             DriverController driverController, 
             Lifter lifter) {
-        DeadzoneWithLinearRemap turboMode = turboMode(driverController, 0.5, 1.0);
         driverController.yRemappers()
                     .addRemapper(deadzone())
                     .addRemapper(throttleCurve())
-                    .addRemapper(turboMode)
-                    .addRemapper(wallDetector(lifter, turboMode.maxPositiveOutput.cloneValue()));
+                    .addRemapper(turboMode(driverController, 0.5, 1.0))
+                    .addRemapper(wallDetector(lifter));
                     
         driverController.xRemappers()
                     .addRemapper(deadzone())
@@ -116,7 +45,7 @@ public class DriverControllerConfig {
         driverController.yawRemappers()
                     .addRemapper(deadzone())
                     .addRemapper(throttleCurve())
-                    .addRemapper(turboMode(driverController, 0.5, 1.0));
+                    .addRemapper(turboMode(driverController, 0.7, 1.0));
 
         return driverController;
     }
